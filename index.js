0f661e79abd68a9ef94f93d97d1e8751d7767bc2
Kevin Sudor <ksudor42@gmail.com>
2018-04-15 19:13:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,76 @@
 var cart = [];
 
 function getCart() {
- return cart;
+	return cart;
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
+	cart = c;
+	return cart;
 }
 
 function addToCart(item) {
- // write your code here
+	var price = function getRandomArbitrary() {
+  		return Math.floor(Math.random() * (100 - 1) + 1);
+	}
+	var object = {};
+
+	Object.assign(object, {
+  		itemName: item,
+  		itemPrice: parseInt(price())
+	});
+
+	cart.push(object);
+
+	return item + " has been added to your cart.";
 }
 
 function viewCart() {
-  // write your code here
+	if (cart.length > 0) {
+		var combo = [];
+
+		for (let i = 0; i < cart.length; i++) {
+			if (cart.length >= 2) {
+				if (i === (cart.length - 1)) {
+					combo[i] = " and " + cart[i].itemName + " at $" + cart[i].itemPrice + ".";
+				} else {
+					combo[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice;
+				}
+			}
+
+			else if (cart.length === 1) {
+				combo[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice + ".";
+			}
+		}
+
+		return `In your cart, you have${combo}`;
+
+	} else if (cart.length === 0) {
+		return "Your shopping cart is empty.";
+	}
 }
 
 function total() {
-  // write your code here
+
+	var combo = [];
+
+	cart.forEach(function(element) {
+  		combo.push(element.itemPrice);
+	});
+
+	var sum = combo.reduce((a, b) => a + b, 0);
+
+	return sum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+	if (cart.includes('item')) {
+		var index = indexOf('item');
+		cart.splice(index, 1);
+		return cart;
+	} else {
+		return "That item is not in your cart.";
+	}
 }
 
 function placeOrder(cardNumber) {
